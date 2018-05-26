c31905af6cf3a407c1c9e28e78087522d7382b60
Sebastiano Gottardo <dextorer@gmail.com>
2018-05-26 18:39:59 +0200

    Fix typo (#2)

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle16.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle16.kt
@@ -5,7 +5,7 @@ import io.reactivex.Single
 
 object Riddle16 {
   /**
-   * For each emission of the [first] Observable use the [function] and return it's value.
+   * For each emission of the [first] Observable use the [function] and return its value.
    * Dispose all previously non terminated returned Singles from the [function] upon receiving a new emission from [first].
    *
    * Use case: The [first] Observable is a TextField and you want to issue a network request while disposing the old requests in case the user has typed something new.
