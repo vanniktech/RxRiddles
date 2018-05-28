1843116d025f7934dd2e361c8cb871e857132778
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-05-28 22:47:30 +0200

    Formatting

--- src/test/kotlin/com/vanniktech/rxriddles/solutions/Riddle101Solution.kt
+++ src/test/kotlin/com/vanniktech/rxriddles/solutions/Riddle101Solution.kt
@@ -5,8 +5,7 @@ import io.reactivex.Single
 import java.util.concurrent.TimeUnit.SECONDS
 
 object Riddle101Solution {
-  fun solve(seconds: Long) = Observable
-        .interval(0, 1, SECONDS)
-        .map { ticksPassed -> seconds - ticksPassed }
-        .takeUntil { it == 0L }
+  fun solve(seconds: Long) = Observable.interval(0, 1, SECONDS)
+      .map { ticksPassed -> seconds - ticksPassed }
+      .takeUntil { it == 0L }
 }
