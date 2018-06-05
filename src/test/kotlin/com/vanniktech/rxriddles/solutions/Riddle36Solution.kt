97f67a739f685652e864205988e003d3bdd99416
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-06-05 12:08:40 +0200

    Format Riddle36Solution

--- src/test/kotlin/com/vanniktech/rxriddles/solutions/Riddle36Solution.kt
+++ src/test/kotlin/com/vanniktech/rxriddles/solutions/Riddle36Solution.kt
@@ -1,11 +1,9 @@
 package com.vanniktech.rxriddles.solutions
 
 import io.reactivex.Observable
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.MILLISECONDS
 
 object Riddle36Solution {
-
-  fun solve(source: Observable<String>, milliseconds: Long): Observable<String> =
-    source.debounce(milliseconds, TimeUnit.MILLISECONDS)
-
+  fun solve(source: Observable<String>, milliseconds: Long)
+      = source.debounce(milliseconds, MILLISECONDS)
 }
