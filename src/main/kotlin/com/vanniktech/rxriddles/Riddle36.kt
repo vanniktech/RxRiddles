01fdc292ff38a7ad2f2273d183e8b52b08c4b603
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-06-05 12:07:21 +0200

    Small adjustments.

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle36.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle36.kt
@@ -4,7 +4,7 @@ import io.reactivex.Observable
 
 object Riddle36 {
   /**
-   * Return an observable that only emits items from [source] if there is not another emission before [milliseconds] has passed.
+   * Return an Observable that only emits items from [source] if there isn't another emission before [milliseconds] has passed.
    *
    * Use case: You want the user-input to trigger a search request for the entered text but only when no changes have been made for a pre-determined time to avoid unnecessary requests.
    */
