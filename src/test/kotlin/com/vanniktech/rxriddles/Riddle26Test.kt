9b4d8edd1edc79d03a215da040bb0560836747d2
Gulzar Ahmed <gulzarhome54@gmail.com>
2018-08-23 22:24:33 +0530

    Fixed Riddle #26
    Test fails when `delaySubscription` is used

--- src/test/kotlin/com/vanniktech/rxriddles/Riddle26Test.kt
+++ src/test/kotlin/com/vanniktech/rxriddles/Riddle26Test.kt
@@ -4,21 +4,29 @@ import com.vanniktech.rxriddles.solutions.Riddle26Solution
 import com.vanniktech.rxriddles.tools.RxRule
 import io.reactivex.Observable
 import io.reactivex.Single
+import org.assertj.core.api.Java6Assertions
 import org.junit.Rule
 import org.junit.Test
 import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.atomic.AtomicInteger
 
 /** Solution [Riddle26Solution] */
 class Riddle26Test {
   @get:Rule val rxRule = RxRule()
 
-  @Test fun solve() {
+    @Test
+    fun solve() {
+    val c = AtomicInteger()
+        
     val source = Observable.just(0L, 1L, 2L)
-        .flatMapSingle { number -> Single.timer(number * 300L, MILLISECONDS, rxRule).map { number } }
+         .flatMapSingle { number -> Single.timer(number * 300L, MILLISECONDS, rxRule).map { number } }
+         .doOnSubscribe { c.incrementAndGet() }
 
     val o = Riddle26.solve(source)
         .test()
         .assertEmpty()
+
+        Java6Assertions.assertThat(c.get()).isEqualTo(1) // Prevents delaying subscription
 
     rxRule.advanceTimeBy(200, MILLISECONDS)
     o.assertEmpty()
