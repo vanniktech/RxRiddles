df494096cbf8bdcae33d2e889c920b8793751069
Gulzar Ahmed <gulzarhome54@gmail.com>
2018-08-25 14:50:11 +0530

    Improve test for Riddle #26 (#18)

--- src/test/kotlin/com/vanniktech/rxriddles/Riddle26Test.kt
+++ src/test/kotlin/com/vanniktech/rxriddles/Riddle26Test.kt
@@ -4,21 +4,28 @@ import com.vanniktech.rxriddles.solutions.Riddle26Solution
 import com.vanniktech.rxriddles.tools.RxRule
 import io.reactivex.Observable
 import io.reactivex.Single
+import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.Rule
 import org.junit.Test
 import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.atomic.AtomicInteger
 
 /** Solution [Riddle26Solution] */
 class Riddle26Test {
   @get:Rule val rxRule = RxRule()
 
   @Test fun solve() {
+    val doOnSubscribe = AtomicInteger()
+
     val source = Observable.just(0L, 1L, 2L)
         .flatMapSingle { number -> Single.timer(number * 300L, MILLISECONDS, rxRule).map { number } }
+        .doOnSubscribe { doOnSubscribe.incrementAndGet() }
 
     val o = Riddle26.solve(source)
         .test()
         .assertEmpty()
+
+    assertThat(doOnSubscribe.get()).isEqualTo(1) // Needs to subscribe immediately.
 
     rxRule.advanceTimeBy(200, MILLISECONDS)
     o.assertEmpty()
