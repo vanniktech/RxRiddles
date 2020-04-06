f530d1cf10e13e2831c520a90fd173fd6b6acecd
Kyle <masteramyx@gmail.com>
2020-04-06 03:08:16 -0400

    wording change (#23)
    Co-authored-by: Kyle Amyx <kyle.amyx@duethealth.com>

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle14.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle14.kt
@@ -4,7 +4,7 @@ import io.reactivex.Single
 
 object Riddle14 {
   /**
-   * Retry the given [source] up to three times unless an [IllegalArgumentException] has been emitted.
+   * Try the given [source] up to three times unless an [IllegalArgumentException] has been emitted.
    *
    * Use case: Retry an operation for a number of times or until a valid error occurred.
    */
