c0cbd4e09b83c615d0fa48307874c5bd1d77b46c
Gustavo Pagani <luizgrp@gmail.com>
2020-05-11 11:28:04 +0100

    Improve solution for riddle 16 and change param names (#24)

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle16.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle16.kt
@@ -5,12 +5,12 @@ import io.reactivex.Single
 
 object Riddle16 {
   /**
-   * For each emission of the [first] Observable use the [function] and return its value.
-   * Dispose all previously non terminated returned Singles from the [function] upon receiving a new emission from [first].
+   * For each emission of the [source] Observable use the [function] and return its value.
+   * Dispose all previously non terminated returned Singles from the [function] upon receiving a new emission from [source].
    *
-   * Use case: The [first] Observable is a TextField and you want to issue a network request while disposing the old requests in case the user has typed something new.
+   * Use case: The [source] Observable is a TextField and you want to issue a network request while disposing the old requests in case the user has typed something new.
    */
-  fun solve(first: Observable<String>, function: (String) -> Single<Int>): Observable<Int> {
+  fun solve(source: Observable<String>, function: (String) -> Single<Int>): Observable<Int> {
     TODO()
   }
 }
